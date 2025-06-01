f8d13a35b87b0ce7b34be505a90cb73b2860a374
rinjugatla <rin.jugatla@gmail.com>
2025-06-01 21:25:11 +0900

    refactor: DataOption型にmeasurementValuesThresholdプロパティの説明コメントを追加

--- src/lib/data_option.d.ts
+++ src/lib/data_option.d.ts
@@ -7,4 +7,9 @@ export type DataOption = {
     existsDatetimeColumn: boolean;
     /** ミリ秒列が存在するか */
     existsMillisecondColumn: boolean;
+    /** 計測値を同値とみなす閾値
+     * 
+     * 重複値除去の際に使用、この値以内は同じ値とみなす
+     */
+    measurementValuesThreshold: number;
 }
