8a6048fc44eb75dd71ae09a63b547279ee133148
rinjugatla <rin.jugatla@gmail.com>
2025-06-01 21:23:41 +0900

    refactor: DataOption型にプロパティの説明コメントを追加

--- src/lib/data_option.d.ts
+++ src/lib/data_option.d.ts
@@ -1,6 +1,10 @@
 export type DataOption = {
+    /** データの区切り文字 */
     delimiter: ',' | '\t';
+    /** ヘッダー行が存在するか */
     existsHeaderRow: boolean;
+    /** 日時列が存在するか */
     existsDatetimeColumn: boolean;
+    /** ミリ秒列が存在するか */
     existsMillisecondColumn: boolean;
 }
